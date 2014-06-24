b6d458e7d3b0dab624f2fcfc90b2ca091c3423e1
Mauricio Almeida <mauriciomdea@gmail.com>
2014-06-24 19:21:06 -0300

    Unit tests for Omniauth

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,29 +3,24 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
 	setup do
-    # 
+		@user_from_linkedin = User.from_omniauth(OmniAuth.config.mock_auth[:linkedin])
+		@user_from_google = User.from_omniauth(OmniAuth.config.mock_auth[:google])
   end
 
   test "should create new user from omniauth hash" do
-  	auth = OmniAuth.config.mock_auth[:linkedin]
-  	User.from_omniauth(auth)
-  	user = User.last
-  	email = ""
-  	auth.slice(:provider, :uid).try do
-  		email = auth.info.email
-  	end
-  	assert_equal email, user.email, "User not found"
+  	assert_equal 1, User.all.size, "Wrong number of total users"
+  	assert_equal "mauriciomdea@gmail.com", @user_from_linkedin.email, "User not found"
 	end
 
-	test "should create new authentication for same email" do
-		auth_from_linkedin = OmniAuth.config.mock_auth[:linkedin]
-		user_from_linkedin = User.from_omniauth(auth_from_linkedin)
-		auth_from_google = OmniAuth.config.mock_auth[:google]
-		user_from_google = User.from_omniauth(auth_from_google)
-		assert_equal user_from_linkedin._id, user_from_google._id, "Different users found"
-		assert_equal 2, user_from_google.authentications.size, "Incorrent number of authentications for user"
-		second_login_from_google = User.from_omniauth(auth_from_google)
-		assert_equal 2, user_from_google.authentications.size, "Incorrent number of authentications for user after second login"
+	test "should create new authentication for same email/user" do
+		assert_equal @user_from_linkedin._id, @user_from_google._id, "Different users found"
+		assert_equal 2, @user_from_google.authentications.size, "Incorrent number of authentications for user"
+	end
+
+	test "should authenticate user with existing authentication" do
+		login_from_google = User.from_omniauth(OmniAuth.config.mock_auth[:google])
+		assert login_from_google, "Did not retrive user from authentication"
+		assert_equal 2, @user_from_google.authentications.size, "Incorrent number of authentications for user after second login"
 	end
 
 end
